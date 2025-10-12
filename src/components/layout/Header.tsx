2e139edbbbf4ad32da9bff7ad4be18c88feaaf27
imranprof <imranpers@gmail.com>
2025-10-13 02:46:03 +0600

    style: modify header text size

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="h-[120px] w-full flex  items-center justify-between px-4 mx-auto">
       <div className="text-3xl font-sofia uppercase font-bold">AniVio</div>
-      <nav className="flex gap-4 font-medium font-sofia text-xl">
+      <nav className="flex gap-4 font-medium font-poppin text-[16px]">
         <Link href={"/"} className="nav-menu-item"><span className="relative  z-[2]">
           Service</span></Link>
         <Link href={"/projects"} className="nav-menu-item"><span className="relative  z-[2]">Projects</span></Link>
