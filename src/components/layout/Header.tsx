dd9c89f32c411d6b73cc5c626cbba15587308174
imranprof <imranpers@gmail.com>
2025-10-14 15:35:59 +0600

    refactor: header nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,15 +17,9 @@ const Header = () => {
   ];
 
   return (
-    <header className={cn("h-[120px] w-full flex  items-center justify-between px-4 mx-auto", pathname === "/projects" && "bg-black")}>
+    <header className={cn("h-[120px] w-full flex  items-center justify-between px-4 container", pathname === "/projects" && "bg-black")}>
       <Link className={cn("text-3xl font-sofia uppercase font-bold", pathname === "/projects" && "text-white")} href={"/"}>AniVio</Link>
       <nav className="flex gap-4 font-medium font-poppin text-[16px]">
-        {/* <Link href={"/"} className="nav-menu-item"><span className="relative  z-[2]">
-          Service</span></Link>
-        <Link href={"/projects"} className="nav-menu-item"><span className="relative  z-[2]">Projects</span></Link>
-        <Link href={"#"} className="nav-menu-item"><span className="relative  z-[2]">About</span></Link>
-        <Link href={"/contact"} className="nav-menu-item"><span className="relative  z-[2]">Contact</span></Link> */}
-
         {
           navItems.map(({ href, label }) => (
             <Link key={href} href={href} className={cn("nav-menu-item", pathname === href &&
