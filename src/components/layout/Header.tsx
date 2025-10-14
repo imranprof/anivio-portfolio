2fe05b779315ef663fb94b099f65c985eec7b8b3
imranprof <imranpers@gmail.com>
2025-10-14 06:42:14 +0600

    style: active nav item bg with pathname

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,15 +9,29 @@ const Header = () => {
 
   const pathname = usePathname();
 
+  const navItems = [
+    { href: "/", label: "Services" },
+    { href: "/projects", label: "Projects" },
+    { href: "/about", label: "About" },
+    { href: "/contact", label: "Contact" },
+  ];
+
   return (
     <header className={cn("h-[120px] w-full flex  items-center justify-between px-4 mx-auto", pathname === "/projects" && "bg-black")}>
       <Link className={cn("text-3xl font-sofia uppercase font-bold", pathname === "/projects" && "text-white")} href={"/"}>AniVio</Link>
       <nav className="flex gap-4 font-medium font-poppin text-[16px]">
-        <Link href={"/"} className="nav-menu-item"><span className="relative  z-[2]">
+        {/* <Link href={"/"} className="nav-menu-item"><span className="relative  z-[2]">
           Service</span></Link>
         <Link href={"/projects"} className="nav-menu-item"><span className="relative  z-[2]">Projects</span></Link>
         <Link href={"#"} className="nav-menu-item"><span className="relative  z-[2]">About</span></Link>
-        <Link href={"/contact"} className="nav-menu-item"><span className="relative  z-[2]">Contact</span></Link>
+        <Link href={"/contact"} className="nav-menu-item"><span className="relative  z-[2]">Contact</span></Link> */}
+
+        {
+          navItems.map(({ href, label }) => (
+            <Link key={href} href={href} className={cn("nav-menu-item", pathname === href &&
+              "text-white before:!opacity-100")}><span className="relative  z-[2]">{label}</span></Link>
+          ))
+        }
       </nav>
       <Button>
         Lets's Talk
