0a284bc7f54ec0bf4ba08008c98689b241ed1c7f
imranprof <imranpers@gmail.com>
2025-10-15 07:32:57 +0600

    feat: add projects data for section

--- src/components/sections/projects/ProjectCard.tsx
+++ src/components/sections/projects/ProjectCard.tsx
@@ -1,21 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 type projectProps = {
-  id: number;
-  title: string;
-  main_img: string;
-  thumb_img: string;
-  year: number;
-  tag: string;
-  slug: string;
+  data: {
+    id: number;
+    title: string;
+    main_img: string;
+    thumb_img: string;
+    year: number;
+    tag: string;
+    slug: string;
+  }
 }
 
-const ProjectCard = () => {
+const ProjectCard = ({ data }: projectProps) => {
+  const { id, title, main_img, thumb_img, year, tag, slug } = data
   return (
-    <div className="relative w-full aspect-square">
+    <Link href={`/projects/${slug}`} className="relative w-full aspect-square">
       <div className="main_img_thumb w-full h-full rounded-md overflow-hidden">
-        <Image src={"/imgs/projects/01hero.jpg"}
+        <Image src={main_img}
           alt="ani-addons"
           height={1280}
           width={1920}
@@ -23,7 +27,7 @@ const ProjectCard = () => {
       </div>
 
       <div className="inside_img_thumb w-[40%] h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <Image src={"/imgs/projects/01hero1.jpg"}
+        <Image src={thumb_img}
           alt="thumb"
           width={1000}
           height={1000}
@@ -31,17 +35,17 @@ const ProjectCard = () => {
       </div>
 
       <div className="description">
-        <div className="date absolute top-[2%] left-[2%] bg-black rounded-md p-1">2025</div>
+        <div className="date absolute top-[2%] left-[2%] bg-black rounded-md p-1">{year}</div>
         <div className="absolute top-1/2 flex justify-between w-full px-[20px]">
           <div className="title ">
-            Animation Addons
+            {title}
           </div>
-          <div className="tag">Web Animation</div>
+          <div className="tag">{tag}</div>
         </div>
       </div>
 
 
-    </div>
+    </Link>
   )
 }
 
--- src/components/sections/projects/ProjectsSection.tsx
+++ src/components/sections/projects/ProjectsSection.tsx
@@ -1,12 +1,15 @@
 import ProjectCard from "@/components/sections/projects/ProjectCard"
+import projectsData from "@/content/projectList.json"
 
 
 const ProjectsSection = () => {
   return (
-    <section className="inner-container grid grid-cols-1 md:grid-cols-2 gap-5">
-
-      <ProjectCard />
-      <ProjectCard />
+    <section className="inner-container grid grid-cols-1 md:grid-cols-2 gap-5 mt-20">
+      {
+        projectsData.map((item) => (
+          <ProjectCard data={item} key={item.id} />
+        ))
+      }
     </section>
   )
 }
