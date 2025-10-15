c51960686636d0008f08431c6a3375b9c9850010
imranprof <imranpers@gmail.com>
2025-10-15 07:11:16 +0600

    style: project card initial style

--- src/components/sections/projects/ProjectCard.tsx
+++ src/components/sections/projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 
 type projectProps = {
@@ -12,21 +13,31 @@ type projectProps = {
 
 const ProjectCard = () => {
   return (
-    <div className="relative ">
-      <div className="main_img_thumb">
-
+    <div className="relative w-full aspect-square">
+      <div className="main_img_thumb w-full h-full rounded-md overflow-hidden">
+        <Image src={"/imgs/projects/01hero.jpg"}
+          alt="ani-addons"
+          height={1280}
+          width={1920}
+          className="w-full h-full object-cover" />
       </div>
 
-      <div className="inside_img_thumb">
-
+      <div className="inside_img_thumb w-[40%] h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Image src={"/imgs/projects/01hero1.jpg"}
+          alt="thumb"
+          width={1000}
+          height={1000}
+          className="w-full h-full object-cover" />
       </div>
 
       <div className="description">
-        <div className="date">2025</div>
-        <div className="title">
-          Animation Addons
+        <div className="date absolute top-[2%] left-[2%] bg-black rounded-md p-1">2025</div>
+        <div className="absolute top-1/2 flex justify-between w-full px-[20px]">
+          <div className="title ">
+            Animation Addons
+          </div>
+          <div className="tag">Web Animation</div>
         </div>
-        <div className="tag">Web Animation</div>
       </div>
 
 
--- src/components/sections/projects/ProjectsSection.tsx
+++ src/components/sections/projects/ProjectsSection.tsx
@@ -3,8 +3,9 @@ import ProjectCard from "@/components/sections/projects/ProjectCard"
 
 const ProjectsSection = () => {
   return (
-    <section className="container">
+    <section className="inner-container grid grid-cols-1 md:grid-cols-2 gap-5">
 
+      <ProjectCard />
       <ProjectCard />
     </section>
   )
