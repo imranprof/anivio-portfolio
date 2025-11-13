66b00e89c833537ec606a2932a402b43c227131a
imranprof <imranpers@gmail.com>
2025-11-14 00:58:37 +0600

    modify medical animation and also add animation only device laptop and up

--- src/app/animations/hasTestAnimation.ts
+++ src/app/animations/hasTestAnimation.ts
@@ -3,66 +3,69 @@ import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger)
 
-export const hasTestAnimation = () =>{
+export const hasTestAnimation = () => {
   const mapContainer = document.querySelector<HTMLElement>("#svgContainer")
   // const allParts = gsap.utils.toArray<SVGElement>("g ", mapContainer);
-  const allParts = gsap.utils.toArray<SVGElement>("g[id^='Group']", mapContainer);
+  const allParts = gsap.utils.toArray<SVGElement>("[id^='Group']", mapContainer);
   const allTexts = gsap.utils.toArray<SVGElement>("g[id^='GTitle']", mapContainer);
 
-  console.log(allParts)
+  // console.log(allParts)
+  console.log("parts-- ", allParts.length)
+
+  const mm = gsap.matchMedia();
+
+  mm.add("(min-width: 1024px)", () => {
+
+    ScrollTrigger.create({
+      trigger: mapContainer,
+      start: "top top",
+      end: "+=1100px",
+      pin: true,
+      // markers: true,
+      scrub: true,
+    });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: mapContainer,
+        start: "top 80%",
+        end: "+=1600px",
+        scrub: true,
+        // markers: true,
+      },
+    });
+
+    tl.from(allParts, {
+      x: i => {
+        const group = i % 4;
+        if (group === 0) return "-800"; // left-top
+        if (group === 1) return "800";  // right-top
+        if (group === 2) return "-800"; // left-bottom
+        return "800";                   // right-bottom
+      },
+      y: i => {
+        const group = i % 4;
+        if (group === 0) return "-800"; // left-top
+        if (group === 1) return "-800"; // right-top
+        if (group === 2) return "800";  // left-bottom
+        return "800";                   // right-bottom
+      },
+      duration: 5,
+      ease: "power1.inOut",
+      stagger: 0.8
+    });
 
 
-ScrollTrigger.create({
-  trigger: mapContainer,
-  start: "top top", // pin starts when top hits 10%
-  end: "+=900px",
-  pin: true,
-  // markers: true,
-  scrub: true,
-});
 
-// Then: Animate elements separately (start later)
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: mapContainer,
-    start: "top 50%", // animation starts when top hits 50%
-    end: "+=1200px",
-    scrub: true,
-    // markers: true,
-  },
-});
+    tl.to(allTexts, {
+      autoAlpha: 0,
+      duration: 0.5,
+      stagger: 0.05,
+      ease: "expo"
+    });
 
-//   allParts.forEach((part, i) => {
-//   tl.from(part, {
-//     x: i % 2 === 0 ? -800 : 800, // alternate direction
-//     // opacity: 0,
-//     // autoAlpha: 0,
-//     duration: 5,
-//     ease: "power1",
-//   });
-// });
 
-tl.from(allParts, {
-  x: i => i % 2 === 0 ? -800 : 800,
-  duration: 5,
-  ease: "power1",
-  stagger: 0.8 // pairs animate at the same time
-});
-
-//   allTexts.forEach((text, i) => {
-//   tl.to(text, {
-//     autoAlpha: 0,
-//     duration: 0.5,
-//   }, );
-// });
-
-  tl.to(allTexts, {
-    autoAlpha: 0,
-    duration: 0.5,
-    stagger: 0.05,
-    ease: "expo"
-  }, );
-
+  });
 
 
 
