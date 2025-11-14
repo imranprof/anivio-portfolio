972f4fa0ecc07ce80e980d68dc819d1aac1f8540
imranprof <imranpers@gmail.com>
2025-11-14 23:11:36 +0600

    feat: add common hero and rotating animation sample page

---
+++ src/app/sample-animation/rotating-section/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import CommonHero from "@/components/common/CommonHero"
+
+const RotatingSection = () => {
+  return (
+    <div>
+      <CommonHero title="Rotating" />
+    </div>
+  )
+}
+
+export default RotatingSection
---
+++ src/components/common/CommonHero.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { ArrowDown, CircleChevronDown } from "lucide-react"
+import { useGSAP } from "@gsap/react"
+import { useRef } from "react"
+import { hasTextReveal } from "@/app/animations/hasTextReveal"
+
+type HeroProps = {
+
+  title: string,
+  firstSubtitle?: string,
+  secondSubtitle?: string,
+  sectionLeft?: string
+}
+
+const CommonHero = ({ title, firstSubtitle, secondSubtitle, sectionLeft }: HeroProps) => {
+  const containerRef = useRef<HTMLElement>(null);
+
+  useGSAP(() => {
+    hasTextReveal();
+  }, { scope: containerRef })
+  return (
+    <section ref={containerRef} className="container flex flex-col font-poppin relative">
+
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center items-center my-20 py-20">
+          <span className="text-[18px] font-medium text-primary/80">{firstSubtitle ? firstSubtitle : "Reach Out"}</span>
+          <h1 className="has-text-reveal text-[clamp(64px,15vw,300px)] font-semibold px-[80px]" >
+            {title}
+          </h1>
+          <span className="text-[18px] font-medium text-primary/80">{secondSubtitle ? secondSubtitle : "Say Hello"}</span>
+        </div>
+        <p className="text-[24px] lg:max-w-[350px] xl:max-w-[350px] font-medium font-poppin text-center lg:absolute lg:bottom-0">Don’t let your vision wait <br /> <span className="font-sofia font-semibold">Let’s make it real</span></p>
+      </div>
+      <div className="hero_bottom text-center flex justify-between inner-container w-full mx-auto">
+        <div className="flex justify-between items-center text-[18px] font-medium"> <ArrowDown className="pe-5 text-black h-10 w-10" /> Scroll To Explore </div>
+        <div className="text-[18px] font-semibold font-poppin flex items-center">{sectionLeft ? sectionLeft : "Imranpers@gmail.com"}</div>
+      </div>
+    </section>
+  )
+}
+
+export default CommonHero
